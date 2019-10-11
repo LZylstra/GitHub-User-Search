84f7dbfbbfc13fade187b3051dd142db59a7d0bd
Ieva Urbaite <ieva@greyepk.com>
2019-10-10 23:20:18 -0400

    created getUserRepos function

--- index.js
+++ index.js
@@ -2,11 +2,26 @@ const apiKey = "";
 const searchURL = "https://api.github.com/users";
 
 
+function displayResults(){
+    console.log("displayResults ran");
+}
 
 
 function getUserRepos(searchTerm){
     console.log(searchTerm);
+    const url = `${searchURL}/${searchTerm}/repos`
+    console.log(url);
+    fetch(url).then(response =>{
+        if(response.ok){
+            return response.json();
+        } throw new Error(reponse.statusText);
+    }) 
+    .then (responseJson => displayResults(responseJson))
+    .catch(err=>{
+        $('#js-error').text(`There was an error. ${err.message}.`);
+    })
 }
+
 
 function watchForm(){
     $('form').submit(event =>{
